c27533f4afb2a47eeee6c529e2171259914676cd
Web Developer <ts.analista@gmail.com>
2023-09-28 09:46:46 -0300

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React Test</h1>
+      <h1>Vite + React Test2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
